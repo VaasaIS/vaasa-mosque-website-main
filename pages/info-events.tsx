8b2c2de913d53b043b927ac53e0eaf7fbac437de
ahmedjanwar <ajemal840@gmail.com>
2025-07-20 21:23:34 +0300

    Refactor RecentArticles and InfoEvents components: removed redundant SVG elements from 'Read More' links for cleaner code. Updated package.json to eliminate duplicate Next.js entry.

--- pages/info-events.tsx
+++ pages/info-events.tsx
@@ -122,20 +122,6 @@ const InfoEvents = ({ events, infoNews }: Props) => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                  <Link href={`/info-news/${info.slug.current}`} className="btn-small inline-flex items-center text-sm md:text-base">
-                    Read More
-                    <svg
-                      className="ml-2 -mr-1 h-4 w-4"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
                   </Link>
                 </div>
               </div>
@@ -219,20 +205,6 @@ const InfoEvents = ({ events, infoNews }: Props) => {
                   <small className="text-sm">{event.venue}</small>
                 </div>
                 <div className="mt-3 flex justify-start md:mt-5">
-                  <Link href={`/event/${event.slug.current}`} className="btn-small inline-flex items-center text-sm md:text-base">
-                    Read More
-                    <svg
-                      className="ml-2 -mr-1 h-4 w-4"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
                   <Link href={`/event/${event.slug.current}`} className="btn-small inline-flex items-center text-sm md:text-base">
                     Read More
                     <svg
